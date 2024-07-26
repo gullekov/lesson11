21d75f0b6b797778271a1ecf561af173b8b1be22
Gullekov Kuvvat <kuveyt94.94@gmail.com>
2024-07-26 15:06:39 +0500

    lesson11

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+ const server = http.createServer((req,res)=>{
+    res.end("Hello Node its me ")
+})
+
+
+server.listen(2000, '127.0.0.1', ()=>{
+    console.log('2000 portdaky maglumat doly yagdayda isledi')
+})
+
+
+
+
