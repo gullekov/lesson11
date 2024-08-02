3f7e710631b359003d048f5ac43db2f486916bfc
Gullekov Kuvvat <kuveyt94.94@gmail.com>
2024-08-02 15:31:32 +0500

    two html add

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+
+
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url, req.method)
+    
+    
+    res.setHeader('Content-type', 'text/html')
+    let pathName = res.url;
+    fs.readFile('./view/index.html', (err,data)=>{
+        if(){
+            console.log(err)
+            res.end()
+        }else{
+            res.end(data)
+        }
+    })
+    
+    
+    res.end();
+    
+})
+
+
+server.listen(3000, 'localhost', ()=>{
+    console.log('3000 port bilen isledildi')
+})
+
+
+
